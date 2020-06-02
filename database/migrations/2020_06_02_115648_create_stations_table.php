786c9795b53f091f7ad65b1a22c41aa8cc5f7006
xdavidwu <xdavidwuph@gmail.com>
2020-06-02 20:14:49 +0800

    model: add stations

---
+++ database/migrations/2020_06_02_115648_create_stations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('name_en');
+            $table->string('area_name');
+            $table->string('county');
+            $table->string('township');
+            $table->string('address');
+            $table->float('longitude', 20, 16);
+            $table->float('latitude', 20, 16);
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stations');
+    }
+}
