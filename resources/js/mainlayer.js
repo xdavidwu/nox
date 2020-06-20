3993b1c01a676ead71b8d679d6839ce3a9e9b705
xdavidwu <xdavidwuph@gmail.com>
2020-06-20 23:39:39 +0800

    view: canvas: main: impl station locking
    click (and tap) will lock a station and make popup always visible
    locked station is rendered black
    click anywhere that is not a station to unlock
    click a station to move lock to that station

--- resources/js/mainlayer.js
+++ resources/js/mainlayer.js
@@ -13,12 +13,14 @@ export default class MainLayer {
         this.popupCoord = { x: 0, y: 0 };
         this.popupVisible = false;
         this.focusedStation = undefined;
+        this.popupLockedStaion = undefined;
 
         for (let station of stations) {
             this.stations.push(new Station(station));
         }
 
         this.canvas.addEventListener('mousemove', this.mouseMove.bind(this));
+        this.canvas.addEventListener('click', this.click.bind(this));
 
         this.resize(scale);
     }
@@ -37,23 +39,28 @@ export default class MainLayer {
         this.ctx.clearRect(0, 0, this.mapInfo.width, this.mapInfo.height);
         this.ctx.fillStyle = 'red';
         for (let station of this.stations) {
-            if (this.focusedStation !== station) {
+            if (station !== this.focusedStation && station !== this.popupLockedStaion) {
                 station.draw(this.ctx, this.mapInfo);
             }
         }
-        if (this.focusedStation) {
-            if ((!this.lastPopup) || this.lastPopupStation !== this.focusedStation) {
-                this.lastPopup = new Popup(this.focusedStation.getText(), 12, this.scale);
-                this.lastPopupStation = this.focusedStation;
+        if (this.popupVisible && ((!this.lastPopup) || this.lastPopupStation !==
+                (this.popupLockedStaion ? this.popupLockedStaion : this.focusedStation))) {
+            this.lastPopupStation = (this.popupLockedStaion ? this.popupLockedStaion : this.focusedStation);
+            this.lastPopup = new Popup(this.lastPopupStation.getText(), 12, this.scale);
 
-                const coord = this.focusedStation.getCoord(this.mapInfo);
-                if (coord.x + this.lastPopup.width > this.mapInfo.width)
-                    this.popupCoord.x = this.mapInfo.width - this.lastPopup.width;
-                else this.popupCoord.x = coord.x;
-                if (coord.y + this.lastPopup.height > this.mapInfo.height)
-                    this.popupCoord.y = coord.y - 16 - this.lastPopup.height;
-                else this.popupCoord.y = coord.y + 16;
-            }
+            const coord = this.lastPopupStation.getCoord(this.mapInfo);
+            if (coord.x + this.lastPopup.width > this.mapInfo.width)
+                this.popupCoord.x = this.mapInfo.width - this.lastPopup.width;
+            else this.popupCoord.x = coord.x;
+            if (coord.y + this.lastPopup.height > this.mapInfo.height)
+                this.popupCoord.y = coord.y - 16 - this.lastPopup.height;
+            else this.popupCoord.y = coord.y + 16;
+        }
+        if (this.popupLockedStaion) {
+            this.ctx.fillStyle = 'black';
+            this.popupLockedStaion.draw(this.ctx, this.mapInfo);
+        }
+        if (this.focusedStation && this.focusedStation !== this.popupLockedStaion) {
             this.ctx.fillStyle = 'blue';
             this.focusedStation.draw(this.ctx, this.mapInfo);
         }
@@ -76,9 +83,28 @@ export default class MainLayer {
             }
         }
         if (!found && this.focusedStation) {
-            this.popupVisible = false;
+            if (!this.popupLockedStaion) this.popupVisible = false;
             this.focusedStation = undefined;
             this.redraw();
+        }
+    }
+
+    click(e) {
+        this.pointerInfo.x = e.offsetX;
+        this.pointerInfo.y = e.offsetY;
+        for (let station of this.stations) {
+            if (station.inRange(this.pointerInfo, this.mapInfo)) {
+                this.popupLockedStaion = station;
+                this.popupVisible = true;
+                this.redraw();
+                return;
+            }
+        }
+        if (this.popupLockedStaion) {
+            this.popupLockedStaion = undefined;
+            this.popupVisible = false;
+            this.redraw();
+            return;
         }
     }
 }
